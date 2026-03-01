6d3fb5979fb70af25ec9d74ad279fcf568a7ec90
Therry1 <therrynganga5@gmail.com>
2026-03-01 08:44:36 +0100

    Fix uplload_file.py

--- firebase.py
+++ firebase.py
@@ -5,9 +5,10 @@ import json
 
 def get_firebase_db():
     if not firebase_admin._apps:
-        # Charge les credentials depuis une variable d'environnement
         cred_json = os.environ.get("FIREBASE_CREDENTIALS")
         cred_dict = json.loads(cred_json)
         cred = credentials.Certificate(cred_dict)
-        firebase_admin.initialize_app(cred)
+        firebase_admin.initialize_app(cred, {
+            "storageBucket": os.environ.get("FIREBASE_STORAGE_BUCKET")
+        })
     return firestore.client()
