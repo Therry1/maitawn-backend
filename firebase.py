4fa9953657a0e6219df25fd3ec9b7914e7546da6
Therry1 <therrynganga5@gmail.com>
2026-03-01 02:54:36 +0100

    Fix firebase vercel (secrets removed)

--- firebase.py
+++ firebase.py
@@ -1,7 +1,22 @@
-import firebase_admin
-from firebase_admin import credentials , firestore
+# import firebase_admin
+# from firebase_admin import credentials , firestore
 
-cred = credentials.Certificate("maitawn-firebase-adminsdk-fbsvc-941841e5af.json")
+# cred = credentials.Certificate("maitawn-firebase-adminsdk-fbsvc-941841e5af.json")
+# firebase_admin.initialize_app(cred)
+
+# get_firebase_db = firestore.client()
+
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+firebase_json = os.getenv("FIREBASE_CREDENTIALS")
+
+cred_dict = json.loads(firebase_json)
+
+cred = credentials.Certificate(cred_dict)
+
 firebase_admin.initialize_app(cred)
 
 get_firebase_db = firestore.client()
