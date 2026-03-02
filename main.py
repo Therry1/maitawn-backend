4ceea230a629ed397eaa962343d9231dae110969
Therry1 <therrynganga5@gmail.com>
2026-03-02 04:28:08 +0100

    fix alert categories

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ from fastapi.responses import JSONResponse
 from app.services.post_alert_management import router as post_alert_management_router
 from app.services.institution_management import router as institution_management_router
 from app.services.authentification_management import router as auth_management_router
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.common import common_router as common_router
 
 logger = logging.getLogger(__name__)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(common_router.router)
 app.include_router(auth_management_router.router)
