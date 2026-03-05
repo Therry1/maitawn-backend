3e53698542409206ca3861d1316d42027c6ac354
Therry1 <therrynganga5@gmail.com>
2026-03-05 13:37:57 +0100

    fix alert categories

--- app/services/post_alert_management/handlers.py
+++ app/services/post_alert_management/handlers.py
@@ -182,8 +182,19 @@ async def make_post_alert (db , payload , attachment):
     )
     
     # envois de la notification
-    #if min_location.get("email"):
-    await send_notification(min_location.get("email"))
+    if min_location.get("user_account_id"):
+        user_account_doc = await asyncio.to_thread(
+            lambda: list(
+                db.collection('users').where(
+                    filter=FieldFilter(
+                        #"category_id", "==", str(payload["post_category_id"])
+                        "id", "==", min_location.get("user_account_id")
+                    )
+                ).stream()
+            )
+        )
+        user = user_account_doc.to_dict()
+        await send_notification(user["email"])
         
     return payload
     # except Exception as exception:
